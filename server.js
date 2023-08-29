7cac3781511828a93b59b72cb066758b0abb4623
Anubhav Garg <garganub@usc.edu>
2023-08-29 18:41:06 -0500

    Add additional fields and calculations, now as json, need additional  changes

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use(express.static(__dirname));  // Serve static files
 
 app.get('/', (req, res) => {
@@ -10,24 +12,58 @@ app.get('/', (req, res) => {
 });
 
 app.post('/calculate', (req, res) => {
-    const purchasePrice = parseFloat(req.body.purchase_price);
-    const downPayment = parseFloat(req.body.down_payment);
-    const monthlyRent = parseFloat(req.body.monthly_rent);
-    // ... get other form data ...
+  const propertyType = req.body['property-type'];
+  const propertySize = req.body['property-size'];
+  const propertyPrice = req.body['property-price'];
+  const downPayment = req.body['down-payment'];
+  const interestRate = req.body['interest-rate'];
+  const closingCosts = req.body['closing-costs'];
+  const insurance = req.body['insurance'];
+  const taxes = req.body['taxes'];
+  const maintenanceCosts = req.body['maintenance-costs'];
+  const propertyManagementFee = req.body['property-management-fee'];
+  const hoaDues = req.body['hoa-dues'];
+  const realtorFee = req.body['realtor-fee'];
+  const otherFees = req.body['other-fees'];
+  const expectedReturn = req.body['expected-return'];
+  const riskTolerance = req.body['risk-tolerance'];
+  const timeHorizon = req.body['time-horizon'];
+  const regulatoryRequirements = req.body['regulatory-requirements'];
+  const fundingSource = req.body['funding-source'];
 
-    // Validation
-    if (isNaN(purchasePrice) || isNaN(downPayment) || isNaN(monthlyRent)) {
-        return res.send('Invalid input. Please ensure all fields are filled correctly.');
-    }
+  // Calculate returns based on input fields
+  const monthlyInvestment = propertyPrice - downPayment;
+  const monthlyReturn = (monthlyInvestment * (interestRate / 12)) / 100;
+  const totalReturn = monthlyReturn * 12;
+  const annualReturn = totalReturn + (totalReturn * (closingCosts / 12));
 
-    const annualRentalIncome = monthlyRent * 12;
-    const totalInvestment = purchasePrice + downPayment;
-    const roi = (annualRentalIncome / totalInvestment) * 100;
-
-    res.send(`ROI: ${roi.toFixed(2)}%`);
+  // Return results as JSON
+  res.json({
+    propertyType,
+    propertySize,
+    propertyPrice,
+    downPayment,
+    interestRate,
+    closingCosts,
+    insurance,
+    taxes,
+    maintenanceCosts,
+    propertyManagementFee,
+    hoaDues,
+    realtorFee,
+    otherFees,
+    expectedReturn,
+    riskTolerance,
+    timeHorizon,
+    regulatoryRequirements,
+    fundingSource,
+    monthlyInvestment,
+    monthlyReturn,
+    totalReturn,
+    annualReturn
+  });
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server started on http://localhost:${PORT}`);
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
 });
